092b596056b01c6cf9eb38c78b2d33d0e335e855
kzw3933 <3099097649@qq.com>
2023-01-04 23:13:53 +0800

    实现类的继承

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -42,10 +42,9 @@ import static com.craftinginterpreters.lox.TokenType.*;
 *  unary      -> ( "!" | "-" ) unary | call ;
 *  call       -> primary ( "(" arguments? ")" | "." IDENTIFIER )* ;
 *  arguments  -> expression ( "," expression )* ;
-*  primary    -> NUMBER | STRING
-*               | "true" | "false" | "nil"
-*               | "(" expression ")"
-*               | IDENTIFIER ;
+*  primary    -> "true" | "false" | "nil" | "this"
+*               | NUMBER | STRING | IDENTIFIER | "(" expression ")"
+*               | "super" "." IDENTIFIER ;
 * */
 
 public class Parser {
@@ -103,13 +102,18 @@ public class Parser {
 
     private Stmt classDeclaration() {
         Token name = consume(IDENTIFIER, "Expect class name.");
+        Expr.Variable superclass = null;
+        if(match(LESS)) {
+            consume(IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
         consume(LEFT_BRACE, "Expect '{' before class body.");
         List<Stmt.Function> methods = new ArrayList<>();
         while(!check(RIGHT_BRACE) && !isAtEnd()) {
             methods.add(function("method"));
         }
         consume(RIGHT_BRACE, "Expect '}' after class body.");
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt varDeclaration() {
@@ -372,6 +376,12 @@ public class Parser {
         if(match(NIL)) return new Expr.Literal(null);
         if(match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
+        }
+        if(match(SUPER)) {
+            Token keyword = previous();
+            consume(DOT, "Expect '.' after 'super'.");
+            Token method = consume(IDENTIFIER, "Expect superclass method name.");
+            return new Expr.Super(keyword, method);
         }
         if(match(THIS)) return new Expr.This(previous());
         if(match(IDENTIFIER)) {
