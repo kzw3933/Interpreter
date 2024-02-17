82d282155530cebb670d2984f5c1aa74d63ed55f
kzw3933 <3099097649@qq.com>
2024-02-17 16:39:32 +0800

    表达式和状态

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -26,6 +26,16 @@ public class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override 
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+
+    @Override 
+    public String visitAssignExpr(Expr.Assign expr) {
+        return parenthesize("=", new Expr.Variable(expr.name), expr.value);
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
         builder.append("(").append(name);
